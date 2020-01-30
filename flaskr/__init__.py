a742cb1c3522ee8a27c4e021e98e8781e3826309
shikuojia <shikuojia@gmail.com>
2020-01-30 20:25:38 +0800

    test ok update

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,6 +7,9 @@ def create_app(test_config = None):
 
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
     
     if test_config is  None:
         app.config.from_pyfile('config.py',silent=True)
