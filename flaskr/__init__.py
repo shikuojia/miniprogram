1eaca3aabc7a3a1b8a2a3556d1623d7a74ba5d60
shikuojia <shikuojia@gmail.com>
2020-01-30 18:40:17 +0800

    update

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,9 @@ def create_app(test_config = None):
     app = Flask(__name__,instance_relative_config = True)
     app.config.from_mapping(SECRET_KEY='dev',DATABASE=os.path.join(app.instance_path,'flaskr.sqlite'))
 
+    from . import db
+    db.init_app(app)
+    
     if test_config is  None:
         app.config.from_pyfile('config.py',silent=True)
     else:
@@ -13,6 +16,8 @@ def create_app(test_config = None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+
 
 
    
