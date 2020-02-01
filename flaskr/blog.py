3960158e9ac49655a6ff36e4a119a4d1a6d8d37f
shikuojia <shikuojia@gmail.com>
2020-02-01 15:28:54 +0800

    update

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -40,3 +40,18 @@ def create():
             db.commit()
             return redirect(url_for('blog.index'))
     return render_template('blog/create.html')
+
+
+def get_post(id,check_author=True):
+    post = get_db().execute(
+        'select p.id,title,body,created,author_id,username'
+        ' from post p JOIN user u ON p.author_id = u.id'
+        ' where p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404,'Post id {0} doesn\'t exist.'.format(id))
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+    return post
